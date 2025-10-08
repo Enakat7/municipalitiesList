0531d0c3314715f0951022a94a5f072b116823ac
enakat <enakat.meat@gmail.com>
2025-10-08 11:13:17 +0900

    feat: フォルダ分け

---
+++ migrations/001_create_municipalities.sql
@@ -0,0 +1,17 @@
+CREATE TABLE municipalities (
+    id SERIAL PRIMARY KEY,
+    prefecture_code VARCHAR(2),
+    prefecture_name VARCHAR(255),
+    prefecture_name_kana VARCHAR(255),
+    prefecture_name_romaji VARCHAR(255),
+    municipality_code VARCHAR(5),
+    municipality_name VARCHAR(255),
+    municipality_name_kana VARCHAR(255),
+    municipality_name_romaji VARCHAR(255),
+    oaza_chochome_name TEXT,
+    oaza_chochome_name_kana TEXT,
+    oaza_chochome_name_romaji TEXT,
+    koaza_common_name TEXT,
+    latitude DECIMAL(9, 6),
+    longitude DECIMAL(10, 7)
+);
---
+++ seeds/07_shikoku/38_ehime_ken.sql
@@ -0,0 +1,2 @@
+INSERT INTO municipalities (prefecture_code, prefecture_name, prefecture_name_kana, prefecture_name_romaji, municipality_code, municipality_name, municipality_name_kana, municipality_name_romaji, oaza_chochome_name, oaza_chochome_name_kana, oaza_chochome_name_romaji, koaza_common_name, latitude, longitude) VALUES
+;
